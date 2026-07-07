c7772f654f5c8adeb80a2cb2cb660925d9bd1e34
Karanveer Singh <karanveersandhu443@gmail.com>
2026-07-08 02:36:18 +0530

    connected and configured postgresql

---
+++ src/test/java/com/spendsense/SpendsenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendsense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendsenseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
