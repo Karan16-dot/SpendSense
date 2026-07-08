09185dff0685fd4b411711503da3369f43a662d6
Karanveer Singh <karanveersandhu443@gmail.com>
2026-07-08 23:05:13 +0530

    feat: add user registration DTOS

---
+++ src/main/java/com/spendsense/user/dto/RegisterResponse.java
@@ -0,0 +1,20 @@
+package com.spendsense.user.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.UUID;
+
+
+@Data
+@Builder
+public class RegisterResponse {
+
+    private UUID id;
+
+    private String fullName;
+
+    private String email;
+
+    private String message;
+}
