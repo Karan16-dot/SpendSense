8f05940fb1422fc02f18e556a9825893cbf95b6f
Karanveer Singh <karanveersandhu443@gmail.com>
2026-07-08 23:47:43 +0530

    feat: implement user registration service

--- src/main/java/com/spendsense/user/service/UserService.java
+++ src/main/java/com/spendsense/user/service/UserService.java
@@ -1,10 +1,39 @@
 package com.spendsense.user.service;
 
+import com.spendsense.user.dto.RegisterRequest;
+import com.spendsense.user.dto.RegisterResponse;
+import com.spendsense.user.entity.User;
+import com.spendsense.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegisterResponse register(RegisterRequest request) {
+
+        if (repository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already registered");
+        }
+
+        User user = User.builder()
+                .fullName(request.getFullName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+        User savedUser = repository.save(user);
+
+        return RegisterResponse.builder()
+                .id(savedUser.getId())
+                .fullName(savedUser.getFullName())
+                .email(savedUser.getEmail())
+                .message("Registration Successful")
+                .build();
+    }
 }
