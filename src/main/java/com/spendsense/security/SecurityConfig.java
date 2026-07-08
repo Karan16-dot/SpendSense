1b08e8fd0c90adbf39bbc5591eb4a186eed0f5e3
Karanveer Singh <karanveersandhu443@gmail.com>
2026-07-09 00:08:56 +0530

    feat: implement registration endpoint and security configuration

--- src/main/java/com/spendsense/security/SecurityConfig.java
+++ src/main/java/com/spendsense/security/SecurityConfig.java
@@ -1,16 +1,35 @@
 package com.spendsense.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 }
