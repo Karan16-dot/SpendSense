fccdcc140d259575977fe3a222e8c6b6cb079f9f
Karanveer Singh <karanveersandhu443@gmail.com>
2026-07-08 20:13:31 +0530

    feat: implement user repository and service layer

--- src/main/java/com/spendsense/user/entity/repository/userRepository.java
+++ src/main/java/com/spendsense/user/entity/repository/userRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public class userRepository extends JpaRepository<User,UUID> {
+public interface userRepository extends JpaRepository<User,UUID> {
 
     Optional<User> findByEmail(String email);
 
