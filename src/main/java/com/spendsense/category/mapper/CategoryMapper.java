005b476911143543b303b137cd257633242cd9e4
Karanveer Singh <karanveersandhu443@gmail.com>
2026-07-10 04:56:24 +0530

    feat(category):implement MapStruct mapper

--- src/main/java/com/spendsense/category/mapper/CategoryMapper.java
+++ src/main/java/com/spendsense/category/mapper/CategoryMapper.java
@@ -1,4 +1,22 @@
 package com.spendsense.category.mapper;
 
-public class CategoryMapper {
+import com.spendsense.category.dto.CategoryRequest;
+import com.spendsense.category.dto.CategoryResponse;
+import com.spendsense.category.dto.CategoryUpdateRequest;
+import com.spendsense.category.entity.Category;
+import com.spendsense.common.mapper.MappingConfig;
+import org.mapstruct.*;
+
+@Mapper(config = MappingConfig.class)
+public interface CategoryMapper {
+
+    Category toEntity(CategoryRequest request);
+
+    CategoryResponse toResponse(Category category);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateCategory(
+            CategoryUpdateRequest request,
+            @MappingTarget Category category
+    );
 }
