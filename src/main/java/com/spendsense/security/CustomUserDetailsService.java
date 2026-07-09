832bbe442d11d6d6d02b06225b078c8ef3c36d5e
Karanveer Singh <karanveersandhu443@gmail.com>
2026-07-10 00:48:42 +0530

    feat: integrate Spring Security with custom user details service

---
+++ src/main/java/com/spendsense/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.spendsense.security;
+
+import com.spendsense.user.entity.User;
+import com.spendsense.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService
+        implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
--- src/main/java/com/spendsense/security/CustomerUserDetails.java
+++
@@ -1,4 +0,0 @@
-package com.spendsense.security;
-
-public class CustomerUserDetails {
-}
